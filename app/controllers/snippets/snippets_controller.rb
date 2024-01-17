c5d2755e1307953048a0c48807f45a7d1b9ee42a
Frank Tennant <frank.tennant@wmg.com>
2024-01-17 10:22:34 +0000

    explicit controller naming convention

--- app/controllers/snippets/snippets_controller.rb
+++ app/controllers/snippets/snippets_controller.rb
@@ -1,4 +1,4 @@
-class Snippets::SnippetsController < JSONAPI::ResourceController
+class SnippetsController < JSONAPI::ResourceController
   skip_before_action :verify_authenticity_token
 
   before_action :authenticate_user!
